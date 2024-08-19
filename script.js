94a545fa8a64c531da761d0a4b021e9f8b7cf17c
Sonam Dolker <141560284+dolker0@users.noreply.github.com>
2024-08-19 20:53:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = bca0c2de9a9638fa34004be2fcfa54b5;
+const apiKey = "bca0c2de9a9638fa34004be2fcfa54b5";
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".input");
@@ -62,3 +62,4 @@ searchBox.addEventListener("keypress", (event) => {
     handleSearch();
   }
 });
+
